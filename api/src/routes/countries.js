3c28311e031007ccd3293735529962523a6a9f90
Pablo Di Pietro <pablod.dipietro@gmail.com>
2022-09-23 09:29:39 -0300

    carga de datos ok

--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -6,22 +6,26 @@ const { Country } = require("../db.js");
 router.get("/", async (req, res) => {
   try {
     const datos = await Country.findAll();
-    if (!datos.length) {
-      console.log(datos);
+    if (datos.length == 0) {
+      
       const paises = await axios("https://restcountries.com/v3/all");
       const countries = paises.data.map((c) => {
         return {
           name: c.name.common != null ? c.name.common : "No se encontro nombre",
-          id: c.cca3,
+          ID: c.cca3,
           flag: c.flags != null ? c.flags[0] : "No se encontro bandera",
-          region: c.region != null ? c.region : "No se encontro region",
+          continent: c.region != null ? c.region : "No se encontro region",
           capital: c.capital != null ? c.capital[0] : "No se encontro capital",
           subregion: c.subregion,
-          area: c.area,
           population: c.population,
+          area: c.area,
         };
       });
       await Country.bulkCreate(countries);
+      
+      res.json(countries)
+      
+      
     } else {
       res.json(datos);
     }
