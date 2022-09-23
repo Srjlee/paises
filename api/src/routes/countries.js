282487d982770a52b936c089ac1261ae9539cea1
Pablo Di Pietro <pablod.dipietro@gmail.com>
2022-09-23 07:31:29 -0300

    trabajando en rutos

--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -1,38 +1,33 @@
 const { Router } = require("express");
 const router = Router();
 const axios = require("axios");
-const { Country} =require('../db.js')
+const { Country } = require("../db.js");
 
-
-router.get('/', async (req, res)=>{
-
-try {
-    const datos = await Country.findAll()
-    if(!datos.length){
-        const paises = await axios("https://restcountries.com/v3/all")
-        const countries = paises.data.map(c=>{
-            return {
-            id: c.fifa,
-            name: c.name.official,
-            continent: c.continent,
-            area: c.area,
-            capital: c.capital, 
-            population: c.population,
-            subregion: c.subregion,
-            flag: c.flags.png
-
-            } 
-            
-        })
-    res.json(countries)
-
+router.get("/", async (req, res) => {
+  try {
+    const datos = await Country.findAll();
+    if (!datos.length) {
+      console.log(datos);
+      const paises = await axios("https://restcountries.com/v3/all");
+      const countries = paises.data.map((c) => {
+        return {
+          name: c.name.common != null ? c.name.common : "No se encontro nombre",
+          id: c.cca3,
+          flag: c.flags != null ? c.flags[0] : "No se encontro bandera",
+          region: c.region != null ? c.region : "No se encontro region",
+          capital: c.capital != null ? c.capital[0] : "No se encontro capital",
+          subregion: c.subregion,
+          area: c.area,
+          population: c.population,
+        };
+      });
+      await Country.bulkCreate(countries);
+    } else {
+      res.json(datos);
     }
-    
-} catch (error) {
-    res.status(404).json(error.message)
-}
-
-    
-})
+  } catch (error) {
+    res.status(404).json(error.message);
+  }
+});
 
 module.exports = router;
